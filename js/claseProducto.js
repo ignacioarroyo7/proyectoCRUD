dd04beed84556fd47b040a3f919ceee6cce008f1
ignacioarroyo7 <ignacioarroyo17@gmail.com>
2021-11-01 02:05:05 -0300

    se realizo la estructura de la pagina admin, su respectivo formulario de carga y tabla donde se mostraran los productos cargados

---
+++ js/claseProducto.js
@@ -0,0 +1,8 @@
+export class Producto{
+    constructor(codigo,nombre,descripcion,url){
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.url = url;
+    }
+}
