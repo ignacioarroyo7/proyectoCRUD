53caa19e038ad93e31ca4dee16060f5b644a5b25
ignacioarroyo7 <ignacioarroyo17@gmail.com>
2021-11-04 18:23:35 -0300

    se realizo el maquetado y diseño de la pagina quienes somos

--- js/animaciones.js
+++ js/animaciones.js
@@ -11,37 +11,6 @@ window.addEventListener('resize', anchoPagina);
 btnRegistrar.addEventListener('click', mostrarRegistroUsuario);
 btnIniciarSesion.addEventListener('click',mostrarIniciarSesion);
 
-//estas de abajo son del index, para el boton que sube arriba y la barra de scroll que se completa mientras baja
-//se ejecuta una funcion cuando todo el contenido del dom se haya cargado
-addEventListener('DOMContentLoaded',()=>{
-    let btn_irArriba=document.querySelector('#ir_arriba_boton');
-    let barra_indicadora=document.querySelector('.indicador_scroll');
-
-    //esta funcion nos devuelve la distancia en px que hay desde el punto actual hasta arriba
-    let obtenerDistancia = () => document.documentElement.scrollTop || document.body.scrollTop;
-
-    let irArriba = ()=>{
-        if(obtenerDistancia() > 0) { //evaluamos si obtenerDistancia>0,
-            requestAnimationFrame(irArriba);
-            scrollTo(0,obtenerDistancia()-(obtenerDistancia()/1));      //nos lleva a una coordenada (x,y);
-        }
-    }
-    let indicarScroll=()=>{
-        if(obtenerDistancia()>50){
-            btn_irArriba.className= 'mostrar';
-        }else{
-            btn_irArriba.className='ocultar';
-        }
-        let alto=document.documentElement.scrollHeight - document.documentElement.clientHeight;
-        let avance_scroll = (obtenerDistancia()/alto)*100;
-        barra_indicadora.style.width=`${avance_scroll}`;
-    };
-
-    btn_irArriba.addEventListener('click', irArriba);
-    window.addEventListener('scroll',indicarScroll);
-});
-
-
 
 function anchoPagina(){
     if(window.innerWidth > 850){
